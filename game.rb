80beeca4cc57bf0dbbdced7daa5d79fd67e6e063
mohamed ali agourram <mohamedaliagourram@gmail.com>
2023-12-31 10:42:27 +0100

    implemented logic

--- game.rb
+++ game.rb
@@ -3,6 +3,7 @@ class Game
   def initialize(players)
     @map = Array.new(3) { Array.new(3) { " " } }
     @players = players
+    @active_player = players[0]
   end
 
   def show_map
@@ -12,10 +13,45 @@ class Game
     end
   end
 
-  def show_scores
-    for player in @players
-      p "#{player.name} #{player.weapon}"
+  def check_pattern
+    # Diagonal checks
+    if @map[0][0] == @map[1][1] && @map[1][1] == @map[2][2] && @map[0][0] != " "
+      return [true, @map[0][0]]
+    elsif @map[0][2] == @map[1][1] && @map[1][1] == @map[2][0] && @map[0][2] != " "
+      return [true, @map[0][2]]
     end
+
+    # Row checks
+    (0..2).each do |i|
+      if @map[i][0] == @map[i][1] && @map[i][1] == @map[i][2] && @map[i][0] != " "
+        return [true, @map[i][0]]
+      end
+    end
+
+    # Column checks
+    (0..2).each do |i|
+      if @map[0][i] == @map[1][i] && @map[1][i] == @map[2][i] && @map[0][i] != " "
+        return [true, @map[0][i]]
+      end
+    end
+
+    [false]
+  end
+
+  def game_ended?
+    if check_pattern[0]
+      @players.each do |player|
+        if player.weapon == check_pattern[1]
+          p "#{player.name} YOU WON!"
+        end
+      end
+      return true
+    end
+    @map.each do |row|
+      return false if row.include?(" ")
+    end
+    true
+    p "GAME ENDED! WITH NO WINNER"
   end
 
   def start_game
@@ -28,6 +64,37 @@ class Game
         @players[1].weapon = weapon == "O" ? "X" : "O"
         break
       end
+    end
+
+    position = []
+    show_map
+    while game_ended? == false
+      row = nil
+      col = nil
+      loop do
+        while !(1..3).include? row
+          print "#{@active_player.name} pick a row:(between 1 and 3) "
+          row = gets.chomp.to_i
+        end
+        while !(1..3).include? col
+          print "#{@active_player.name} pick a column:(between 1 and 3) "
+          col = gets.chomp.to_i
+        end
+        position << row - 1
+        position << col - 1
+        if @map[position[0]][position[1]] == " "
+          break
+        else
+          p "POSITION NOT EMPTY!"
+          row = nil
+          col = nil
+          position = []
+        end
+      end
+      @map[position[0]][position[1]] = @active_player.weapon
+      position = []
+      @active_player = @active_player == @players[0] ? @players[1] : @players[0]
+      show_map
     end
   end
 end
--- main.rb
+++ main.rb
@@ -4,4 +4,3 @@ require_relative "game"
 
 game = Game.new([Player.new, Player.new(2)])
 game.start_game
-game.show_scores
